c7011798367afdf635122e8ccb2f9392feb2bd9f
naraic <naraic0o@gmail.com>
2018-12-03 18:03:12 +0000

    2017 day 3 part 1 done. part 2 is gonna be a pain

--- 2017/3/three.py
+++ 2017/3/three.py
@@ -1,12 +1,26 @@
 #find the odd square less than your number and work from there
 
-from math import floor, sqrt
+from math import ceil, sqrt
 
-n = int(read(open("input")).strip())
-s = floor(sqrt(n))
-if s % 2 == 0:
-#even
-  s -= 1
+n = int(open("input").read().strip())
 
-s*s
+def spiral(n):
+  if n == 1:
+    return 0
+  closest_sq = ceil(sqrt(n))
+  if closest_sq % 2 == 0:
+    closest_odd_sq = cloest_sq + 1
+  else:
+    closest_odd_sq = closest_sq
+  edge = closest_odd_sq - 1
+  inner_len = (edge-1)**2 
+  half_edge = edge // 2
+  offset = n - inner_len
+  pos = offset % edge 
+  if pos > half_edge:
+    difference = half_edge - (pos % half_edge)
+  else:
+    difference = pos
+  print(closest_sq - difference - 1)
 
+spiral(n)
